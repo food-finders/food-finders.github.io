c35d08bf20a5d3c7b1df47c28340dab5459a684b
HRaeder42 <59632748+HRaeder42@users.noreply.github.com>
2020-02-19 10:31:47 -0600

    Added map, commented out images

--- javascript.js
+++ javascript.js
@@ -18,23 +18,44 @@ const getFood = (term) => {
     })
     .then((myJson) => {
       console.log(myJson);
-      let i = 0;
-      let html = ""
-      if (myJson.length == 0) {
-        html = `<p>No Food or Places found.</p>`;
-      }
-      while (i < 10 && i < myJson.length){
-        let restaurant = myJson[i];
-        let addition = `<section class="results">
-                          <h2>${restaurant.name}</h2>
-                          <h2>${restaurant.display_address}</h2>
-                          <h3>Rating: ${restaurant.rating}</h3>
-                          <h3>Price: ${restaurant.price}</h3>
-                          <img src="${restaurant.image_url}">
-                        </section>`
-        html = html + addition;
-        i = i + 1;
-      }
+      const center = [
+        myJson[0].coordinates.latitude,
+        myJson[0].coordinates.longitude
+    ];
+    // initialize map:
+    const mymap = L.map('mapid').setView(center, 11);
+
+    // add basemap:
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+            maxZoom: 18,
+            id: 'mapbox.streets'
+        }).addTo(mymap);
+
+    // add markers:
+    for (restaurant of myJson) {
+        const marker = L.marker([restaurant.coordinates.latitude, restaurant.coordinates.longitude]).addTo(mymap);
+        marker.bindPopup(`
+            <b>${restaurant.name}!</b><br>
+            ${restaurant.display_address}
+        `).openPopup();
+    }
+      // let i = 0;
+      // let html = ""
+      // if (myJson.length == 0) {
+      //   html = `<p>No Food or Places found.</p>`;
+      // }
+      // while (i < 10 && i < myJson.length){
+      //   let restaurant = myJson[i];
+      //   let addition = `<section class="results">
+      //                     <h2>${restaurant.name}</h2>
+      //                     <h2>${restaurant.display_address}</h2>
+      //                     <h3>Rating: ${restaurant.rating}</h3>
+      //                     <h3>Price: ${restaurant.price}</h3>
+      //                     <img src="${restaurant.image_url}">
+      //                   </section>`
+      //   html = html + addition;
+      //   i = i + 1;
+      // }
       document.getElementById("search_results").innerHTML = html;
     });
 };
