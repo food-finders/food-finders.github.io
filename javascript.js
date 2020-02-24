59c2f7f33b4b83f481bd722753c2c114479be9c6
wongsharon <sharonwong2021@u.northwestern.edu>
2020-02-23 18:24:05 -0600

    styling changes

--- javascript.js
+++ javascript.js
@@ -1,26 +1,24 @@
 const search = (ev) => {
     const term = document.querySelector('#search').value;
-    const term2 = document.querySelector('#search2').value;
-    console.log('search for:', term, term2);
+    console.log('search for:', term);
     // issue three Spotify queries at once...
-    getFood(term, term2);
+    getFood(term);
     if (ev) {
         ev.preventDefault();
     }
 }
 
-const getFood = (term, term2) => {
-  const url = `https://www.apitutor.org/yelp/v3/businesses/search?term=${term2}&location=${term}`;
+const getFood = (term) => {
+  const url = `https://www.apitutor.org/yelp/simple/v3/businesses/search?term=${term}&location=Evanston, IL`;
   fetch(url)
     .then((response) => {
       return response.json();
     })
     .then((myJson) => {
       console.log(myJson);
-      data = myJson.businesses;
       const center = [
-        data[0].coordinates.latitude,
-        data[0].coordinates.longitude
+        myJson[0].coordinates.latitude,
+        myJson[0].coordinates.longitude
     ];
     // initialize map:
     var container = L.DomUtil.get('mapid');
@@ -37,24 +35,27 @@ const getFood = (term, term2) => {
 
     // add markers:
     let i = 0;
-    let html = "<h3>Results:<h3>";
+    /*let html = "<h3>Results:</h3>";*/
+    let html = "";
 
-    if (data.length == 0) {
-      html = html + `<p>No Food found.</p>`;
+    if (myJson.length == 0) {
+      html = html + `<p>No Food or Locations found.</p>`;
     }
-    while (i < 10 && i < data.length) {
-        let restaurant = data[i];
+    while (i < 10 && i < myJson.length) {
+        let restaurant = myJson[i];
         const marker = L.marker([restaurant.coordinates.latitude, restaurant.coordinates.longitude]).addTo(mymap);
         marker.bindPopup(`
             <b>${restaurant.name}!</b><br>
             ${restaurant.display_address}
         `).openPopup();
-        let addition = `<section id="restaurant_${i}" class="results">
+        let addition = `<section id="restaurant_${i}" class="restaurant">
                           <h3>${restaurant.name}</h3>
                           <h4>${restaurant.display_address}</h4>
                           <h5>Rating: ${restaurant.rating}</h5>
                           <h5>Price: ${restaurant.price}</h5>
-                          <img src="${restaurant.image_url}">
+                          <!--img src="${restaurant.image_url}" -->
+                          <div class="pic" style="background-image: url('${restaurant.image_url}');">
+</div>
                         </section>`;
         html = html + addition;
         i = i + 1;
@@ -67,7 +68,7 @@ document.getElementById("search_button").onclick = (ev) => {
   search();
 };
 
-document.querySelector('#search2').onkeyup = (ev) => {
+document.querySelector('#search').onkeyup = (ev) => {
      console.log(ev.keyCode);
     if (ev.keyCode === 13) {
         ev.preventDefault();
